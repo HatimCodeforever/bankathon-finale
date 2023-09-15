84078aabbe2ee2f512d1160b1188e2e12cf16e64
unknown <kamblivedant50@gmail.com>
2023-09-15 15:59:42 +0530

    Convert DB to CSV

--- serv/app.py
+++ serv/app.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 app = Flask(__name__)
 
-def get_db_connection():
+def get_db_connection(): #Getting the Database
     conn = sqlite3.connect('data_KRA.sqlite')
     conn.row_factory = sqlite3.Row
     return conn
@@ -23,6 +23,56 @@ def hello_world():
     conn.close()
     print(record['name'])
     return "<p>Hello, Hatim!</p>"
+
+def db_to_csv:
+    import sqlite3
+    import csv
+
+    # Connect to the SQLite database
+    conn = sqlite3.connect('/content/data_KRA.sqlite')  # Replace with the path to your SQLite database file
+    cursor = conn.cursor()
+
+    # Get the list of table names in the database
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
+    table_names = cursor.fetchall()
+
+    # Close the database connection
+    conn.close()
+
+    # Iterate through each table and export to a CSV file
+    for table_name in table_names:
+        table_name = table_name[0]  # Extract the table name from the tuple
+        
+        # Connect to the SQLite database again
+        conn = sqlite3.connect('/content/data_KRA.sqlite')
+        cursor = conn.cursor()
+        
+        # Query data from the table
+        cursor.execute(f'SELECT * FROM {table_name}')
+        
+        # Fetch all the rows
+        rows = cursor.fetchall()
+        
+        # Get the column names from the table
+        cursor.execute(f'PRAGMA table_info({table_name})')
+        column_names = [column[1] for column in cursor.fetchall()]
+        
+        # Close the database connection
+        conn.close()
+        
+        # Create a CSV file and write data
+        csv_file = f'{table_name}.csv'
+        with open(csv_file, 'w', newline='') as file:
+            writer = csv.writer(file)
+            
+            # Write the column headers
+            writer.writerow(column_names)
+            
+            # Write the data rows
+            writer.writerows(rows)
+        
+        print(f'{csv_file} has been created.')
+
 
 # @app.route("/aws-request")
 # def runpodApi():
