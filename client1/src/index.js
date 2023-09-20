36ad280975d75793570b2fd6413d0728bd1ffaa6
Haadi Rakhangi <127193364+haadirakhangi@users.noreply.github.com>
2023-09-20 18:52:53 +0530

    best front end dev

--- client1/src/index.js
+++ client1/src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './views/home';
-
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Home from './views/home';
+import Chatui from './components/Csidebar';
+import ChatInterface from './components/ChatInterface';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
    <BrowserRouter>
     <Routes>
-      <Route path="/home" element={<Home />} />
-
-
+      <Route path="/home" element={<Home/>} />
       <Route path="*" element={<Navigate to="/home" replace />} />
+      <Route path="/chat" element={<Chatui/>} />
     </Routes>
   </BrowserRouter>
   </React.StrictMode>
